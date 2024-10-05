0389aa2bda686fc2dc6ae67481b77d79114a1d34
Bartolomeu Spegiorin Gusella <ccbarto@gmail.com>
2024-10-05 18:27:05 -0300

    add exercises from 9th class

---
+++ lista9/src/Exercicio1.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.Buffer;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        final String FILE_NAME = "arquivo.csv";
+         BufferedReader arquivo = new BufferedReader(FileReader(FILE_NAME));
+    }
+}
